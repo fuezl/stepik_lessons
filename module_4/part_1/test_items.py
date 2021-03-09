157afdc3b3b3771a448b7959acd4bc58ea5eada3
Александр Грачев <A.grachev94@yandex.ru>
2021-03-09 21:19:41 +0300

    добавил conftest и первый тест

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,20 +1,28 @@
 from selenium.webdriver.common.by import By
 
+product_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
 
 def test_add_to_cart_display(browser):
     # Data
-    dict = {'de': 'In Warenkorb legen', 'en-gb': 'Add to basket', 'es': 'Añadir al carrito',
-            'fr': 'Ajouter au panier', 'it': 'Aggiungi al carrello', 'pt': 'Adicionar ao carrinho',
-            'ru': 'Добавить в корзину', 'uk': 'Додати в кошик'}
-    add_to_cart = (By.CSS_SELECTOR, '.product_main [type="submit"]')
+    lang = {'de': 'In Warenkorb legen',
+            'en-gb': 'Add to basket',
+            'es': 'Añadir al carrito',
+            'fr': 'Ajouter au panier',
+            'it': 'Aggiungi al carrello',
+            'pt': 'Adicionar ao carrinho',
+            'ru': 'Добавить в корзину',
+            'uk': 'Додати в кошик'
+            }
+    add_to_cart_locator = (By.CSS_SELECTOR, '.product_main [type="submit"]')
 
     # Arrange
-    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    add_to_cart_text = dict.get(browser.current_url.split("/")[3])
+    browser.get(product_link)
+    expected_btn_text = lang.get(browser.user_language)
 
     # Act
-    cart = browser.find_element(*add_to_cart).text
+    cart_text = browser.find_element(*add_to_cart_locator).text
 
     # Assert
-    assert cart == add_to_cart_text, f"На кнопке добавления в корзину отображается текст {cart!r}, а должен " \
-                                     f"{add_to_cart_text!r}"
+    assert cart_text == expected_btn_text, f"На кнопке добавления в корзину отображается текст {cart_text !r}, " \
+                                           f"а должен {expected_btn_text!r}"
